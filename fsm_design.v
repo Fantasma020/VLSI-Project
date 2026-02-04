95f05a4894aeb56d5d576ddf83f5a89e26a980bb
jesuspalacyos <jesus.eli.p@outlook.com>
2026-02-04 15:13:53 -0600

    Initial design for FSM
    This is the initial design code with only one case. Needs to have other cases and synthesized. tb file still needs to be done to test out the code and the waveforms to complete our report.

---
+++ fsm_design.v
@@ -0,0 +1,45 @@
+module fsm(
+  input  logic       clk,
+  input  logic       reset,
+  input  logic       cash_inserted,
+  input  logic       card_inserted,
+  input  logic [1:0] selection,
+  input  logic [3:0] cashval,
+  output logic       dispense
+);
+
+parameter [3:0]
+  IDLE      = 4'b0000,
+  CASH      = 4'b0001,
+  VERIFY    = 4'b0010,
+  BALANCE   = 4'b0011,
+  SELECTION = 4'b0100,
+  SEL1      = 4'b0101,
+  SEL2      = 4'b0110,
+  SEL3      = 4'b0111,
+  DISPENSE  = 4'b1000;
+
+reg [3:0] state, nextstate;
+
+always @(posedge clk or posedge reset) begin
+  if (reset)
+    state <= IDLE;
+  else
+    state <= nextstate;
+end
+
+always @(*) begin
+  nextstate = state;
+  case (state)
+    IDLE: begin
+      if (cash_inserted || card_inserted)
+        nextstate = CASH;  
+    end
+
+    default: begin
+      nextstate = IDLE;
+    end
+  endcase
+end
+
+endmodule
