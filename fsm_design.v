fa93977188e2f634316bce382a0d9f15a8209508
Fernando <fernihernandez04@gmail.com>
2026-02-05 17:12:43 -0600

    Implement balance tracking and state updates
    Added balance register to track user cash input and updated state transitions based on cash insertion and balance conditions.

--- fsm_design.v
+++ fsm_design.v
@@ -21,19 +21,66 @@ parameter [3:0]
 
 reg [3:0] state, nextstate;
 //state register: basically remembers what state we're in
+  reg [3:0] balance; // most the user can insert is $15
 always @(posedge clk or posedge reset) begin
   if (reset)
     state <= IDLE;
   else
     state <= nextstate;
 end
+//cash insertion counting
+  always @(posedge clk or posedge reset) begin
+    if(reset)
+      balance <= 4'b0000;
+    else if(state == CASH && cash_inserted)
+      balance <= cashval + balance;
+    else if(state == IDLE)
+      balance <= 4'b0000;
+  end
+  
 //next state logic. this is what needs to be worked on the most
 always @(*) begin
   nextstate = state;
   case (state)
     IDLE: begin
       if (cash_inserted || card_inserted)
-        nextstate = CASH;  
+        nextstate = CASH;
+      else
+        nextstate = IDLE;
+    end
+    CASH: begin
+      if(!cash_inserted) //we stopped putting in money
+        nextstate = VERIFY;
+      else
+        nextstate = CASH;
+    end
+    VERIFY: begin
+      if(balance < 5 || balance > 15) // needs $5 to operate and must be below $15 to operate
+        nextstate = DISPENSE;
+      else
+        nextstate = SELECTION;
+    end
+    SELECTION: begin
+      if(selection = 2'b00) // first selection
+        nextstate = SEL1;
+      else if(selection = 2'b01) // user chose second selection
+        nextstate = SEL2;
+      else if (selection = 2'b10) // user chooses third selection
+        nextstate = SEL3;
+      else                // otherwise the user must choose a proper selection
+        nextstate = SELECTION;
+    end
+    SEL1: begin
+      nextstate = DISPENSE;
+    end
+    SEL2: begin
+      nextstate = DISPENSE;
+    end
+    SEL3: begin
+      nextstate = DISPENSE;
+    end
+    DISPENSE: begin
+      nextstate = IDLE;
     end
 
     default: begin
