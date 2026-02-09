2fd20e4af2f4428245f714754a8ca9b5d279be13
Fernando <fernihernandez04@gmail.com>
2026-02-09 02:33:56 -0600

    Refactor FSM design for cash handling logic

--- fsm_design.v
+++ fsm_design.v
@@ -1,11 +1,11 @@
 module fsm(clk,
-		   reset, 
-		   cash_inserted, 
-		   selection, 
-		   cashval, 
-		   dispense
-		  );
-	//port declaration
+           reset, 
+           cash_inserted, 
+           selection, 
+           cashval, 
+           dispense
+          );
+    //port declaration
 input  clk;
 input  reset;
 input  cash_inserted;
@@ -22,86 +22,82 @@ parameter [3:0]
   SEL2      = 4'b0110,
   SEL3      = 4'b0111,
   DISPENSE  = 4'b1000;
-  REFUND    = 4'b1001;
 
-	reg [3:0] state, nextstate, refundval, balance;
+reg [3:0] state, nextstate;
 //state register: basically remembers what state we're in
-   // most the user can insert is $15
+  reg [3:0] balance; // most the user can insert is $15
 always @(posedge clk) begin
   if (reset)
     state <= IDLE;
-	balance <= 4'b0000;
-	cash_inserted = 0;
-    selection = 2'b00;
-    cashval = 4'b0000;
   else
     state <= nextstate;
 end
+    
 
-	always(@posedge clk) begin
-		balance = cashval + balance;
-	end
-	  
+//cash insertion counting
+  always @(posedge clk) begin
+    if(reset)
+      balance <= 4'b0000;
+    else if(state == CASH && cash_inserted)    //This is what was not triggering before sequentially
+      balance <= cashval + balance;
+    else if(state == IDLE)
+      balance <= 4'b0000;
+  end
+  
 //next state logic. this is what needs to be worked on the most
 always @(*) begin
   nextstate = state;
   case (state)
     IDLE: begin
-		if (cash_inserted ) //once cash is inserted then we go into the next state
-        nextstate <= CASH;  
+        if (cash_inserted) //once cash is inserted then we go into the next state
+        nextstate = CASH;  
       else // otherwise we stay at IDLE
-        nextstate <= IDLE;
+        nextstate = IDLE;
     end
     CASH: begin
-		balance = cashval + balance;
-		if(!cash_inserted) //we stopped putting in money //How do we make sure this doesnt eat any input, is cash_inserted boolean? cash_inserted is boolean 
-        nextstate <= VERIFY;
+        if(!cash_inserted) //we stopped putting in money //How do we make sure this doesnt eat any input, is cash_inserted boolean? cash_inserted is boolean 
+        nextstate = VERIFY;
       else // if user is still inserting cash then stay in CASH state
-        nextstate <= CASH;
+        nextstate = CASH;
     end
-    VERIFY: begin		//!!!What happens when the user inputs more money during the verify state and balance hasnt been updated yet because it hasnt went to CASH state? ex: put $5, how does line 35 trigger again, This should be "if it means the minimum, then selection is available" otherwise  return change and 
-		if(balance < 5 )		//When balance is < 5, Insufficient funds, needs to go back to CASH until minimum is met
-        nextstate <= CASH;
-		else if (balance > 5 )    //When balance is > 5, Balance limit breached, Refund excess change and update balance according to returned change [Keep balance at 5]
- 	nextstate <= REFUND;    
-		else if (!cash_inserted)	       //Balance meets minimum and doesnt breach limit, proceed with SELECTION
-	nextstate <= SELECTION; 
+    VERIFY: begin        //What happens when the user inputs more money during the verify state and balance hasn't been updated yet because it hasnt went to CASH state? ex: put $5, how does line 35 trigger again, This should be "if it means the minimum, then selection is available" otherwise  return change and 
+        if(balance < 5 )        //When balance is < 5, Insufficient funds, needs to go back to CASH until minimum is met
+        nextstate = IDLE;
+           else if (balance >= 5)    //When balance is > 5, Balance limit breached, Refund excess change and update balance according to returned change [Keep balance at 5]
+     nextstate = SELECTION; 
     end
-	REFUND : begin
-		//return money
-		refundval = balance - 5;
-		balance = 5;
-		nextstate <= VERIFY;
-	end
     SELECTION: begin
-		if(selection == 2'b01) // first selection
-        nextstate <= SEL1;
-		else if(selection == 2'b10) // user chose second selection
-        nextstate <= SEL2;
-		else if (selection == 2'b11) // user chooses third selection
-        nextstate <= SEL3;
+               if(selection == 2'b01) // first selection
+        nextstate = SEL1;
+               else if(selection == 2'b10) // user chose second selection
+        nextstate = SEL2;
+               else if (selection == 2'b11) // user chooses third selection
+        nextstate = SEL3;
       else                // otherwise the user must choose a proper selection
-        nextstate <= SELECTION;
+        nextstate = SELECTION;
     end
-	  //after each selection we got to dispense no matter what
+      //after each selection we got to dispense no matter what
     SEL1: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
     SEL2: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
     SEL3: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
-	  //We "dispense" the item the user selected then go back to idle
+      //We "dispense" the item the user selected then go back to idle
     DISPENSE: begin
-      nextstate <= IDLE;
+      nextstate = IDLE;
     end
 
     default: begin
-      nextstate <= IDLE;
+      nextstate = IDLE;
     end
   endcase
 end
+
+
+   assign dispense = (state == DISPENSE);
 
 endmodule
