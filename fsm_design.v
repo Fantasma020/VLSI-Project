aa8da3a1e9642ccee1312df23e8e04683156393b
jesuspalacyos <jesus.eli.p@outlook.com>
2026-02-09 01:15:32 -0600

    Refactor state transition logic in FSM

--- fsm_design.v
+++ fsm_design.v
@@ -21,6 +21,7 @@ parameter [3:0]
 reg [3:0] state, nextstate;
 //state register: basically remembers what state we're in
   reg [3:0] balance; // most the user can insert is $15
+
 always @(posedge clk) begin
   if (reset)begin
     state <= IDLE;
@@ -29,57 +30,64 @@ end
   else
     state <= nextstate;
 end
+//balance register
+always @(posedge clk)begin
+	if(reset)
+	balance<=4'd0;
+	else if (state ==IDLE)
+	balance <=4'd0;
+	else if (state ==CASH && cash_inserted)begin
+	if (balance+cashval >=4d5) balance <= 4'd5;
+	else 
+	balance <= balance + cashval;
+	end
+end
 	  
 //next state logic. this is what needs to be worked on the most
 always @(*) begin
   nextstate = state;
   case (state)
     IDLE: begin
-		if (cash_inserted ) //once cash is inserted then we go into the next state
-        nextstate <= CASH;  
-      else // otherwise we stay at IDLE
-        nextstate <= IDLE;
+	if (cash_inserted ) //once cash is inserted then we go into the next state
     end
     CASH: begin
-		balance = cashval + balance;
 		if(!cash_inserted) //we stopped putting in money //How do we make sure this doesnt eat any input, is cash_inserted boolean? cash_inserted is boolean 
-        nextstate <= VERIFY;
-      else // if user is still inserting cash then stay in CASH state
-        nextstate <= CASH;
+        nextstate = VERIFY;
     end
     VERIFY: begin		//What happens when the user inputs more money during the verify state and balance hasnt been updated yet because it hasnt went to CASH state? ex: put $5, how does line 35 trigger again, This should be "if it means the minimum, then selection is available" otherwise  return change and 
-		if(balance < 5 )		//When balance is < 5, Insufficient funds, needs to go back to CASH until minimum is met
-        nextstate <= CASH;  
-		else if (!cash_inserted)	       //Balance meets minimum and doesnt breach limit, proceed with SELECTION
-	nextstate <= SELECTION; 
+		if(balance < 4'd5 )		//When balance is < 5, Insufficient funds, needs to go back to CASH until minimum is met
+        nextstate = CASH;  
+		else 	       //Balance meets minimum and doesnt breach limit, proceed with SELECTION
+	nextstate = SELECTION; 
     end
 	SELECTION: begin
 		if(selection == 2'b01) // first selection
-        nextstate <= SEL1;
+        nextstate = SEL1;
 		else if(selection == 2'b10) // user chose second selection
-        nextstate <= SEL2;
+        nextstate = SEL2;
 		else if (selection == 2'b11) // user chooses third selection
-        nextstate <= SEL3;
+        nextstate = SEL3;
       else                // otherwise the user must choose a proper selection
-        nextstate <= SELECTION;
+        nextstate = SELECTION;
     end
+
 	  //after each selection we got to dispense no matter what
     SEL1: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
     SEL2: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
     SEL3: begin
-      nextstate <= DISPENSE;
+      nextstate = DISPENSE;
     end
 	  //We "dispense" the item the user selected then go back to idle
     DISPENSE: begin
-      nextstate <= IDLE;
+      nextstate = IDLE;
     end
 
     default: begin
-      nextstate <= IDLE;
+      nextstate = IDLE;
     end
   endcase
 end
